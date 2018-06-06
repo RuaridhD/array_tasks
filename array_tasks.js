92d83f05aa167a682136c2ff73c835594b879b52
Ruaridh <ruaridhdunbar@gmail.com>
2018-06-06 16:10:53 +0100

    passes tests to concatenate two arrays, and insert item into array at given position

---
+++ array_tasks.js
@@ -0,0 +1,38 @@
+var arrayTasks = {
+
+	concat: function (arr1, arr2) {
+		return arr1.concat(arr2);
+	},
+
+	insertAt: function (arr, itemToAdd, index) {
+		arr.splice(index, 0, itemToAdd);
+		return arr;
+	},
+
+	// square: function (arr) {
+
+	// },
+
+	// sum: function (arr) {
+
+	// },
+
+	// findDuplicates: function (arr) {
+
+	// },
+
+	// removeAndClone: function (arr, valueToRemove) {
+
+	// },
+
+	// findIndexesOf: function (arr, itemToFind) {
+
+	// },
+
+	// sumOfAllEvenNumbersSquared: function (arr) {
+
+	// }
+
+}
+
+module.exports = arrayTasks
