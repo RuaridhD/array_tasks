3c69a0b005675c8a6c1cb219ac928f9994c42731
Ruaridh <ruaridhdunbar@gmail.com>
2018-06-06 21:42:01 +0100

    got all tests to pass

--- array_tasks.js
+++ array_tasks.js
@@ -9,29 +9,57 @@ var arrayTasks = {
 		return arr;
 	},
 
-	// square: function (arr) {
+	square: function (arr) {
+		var new_array = [];
+		arr.forEach(number => {
+			new_array.push(number*number);
+		})
+		return new_array;
+	},
 
-	// },
+	sum: function (arr) {
+		var total = 0;
+		arr.forEach(number => {
+			total += number;
+		})
+		return total;
+	},
 
-	// sum: function (arr) {
+	findDuplicates: function (arr) {
+		duplicate_values = [];
+		arr.forEach(function(element, index){
+			if (arr.indexOf(element, index + 1) > -1) {
+				if (duplicate_values.indexOf(element) === -1) {
+        duplicate_values.push(element);
+      }
+    }
+  });
+  return duplicate_values;
+},
 
-	// },
+	removeAndClone: function (arr, valueToRemove) {
+	var new_array = arr.filter(function(element){
+			return element !== valueToRemove;
+	});
+	return new_array;
+},
 
-	// findDuplicates: function (arr) {
+	findIndexesOf: function (arr, itemToFind) {
+		var index_array = [];
+		for(i = 0; i < arr.length; i++)
+        if (arr[i] === itemToFind)
+            index_array.push(i);
+    return index_array;
+	},
 
-	// },
-
-	// removeAndClone: function (arr, valueToRemove) {
-
-	// },
-
-	// findIndexesOf: function (arr, itemToFind) {
-
-	// },
-
-	// sumOfAllEvenNumbersSquared: function (arr) {
-
-	// }
+	sumOfAllEvenNumbersSquared: function (arr) {
+		total = 0;
+		for(i = 0; i < arr.length; i++)
+			if (arr[i] % 2 === 0){
+				total += (arr[i] * arr[i]);
+			}
+			return total;
+	}
 
 }
 
